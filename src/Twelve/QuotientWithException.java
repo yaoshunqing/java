3f368719e06415f472a8be0825bd1ad9664bf9b3
yaoshunqing <yaoshunqing0106@gmail.com>
2019-07-09 17:58:25 +0800

    The 12th chapter

---
+++ src/Twelve/QuotientWithException.java
@@ -0,0 +1,31 @@
+package Twelve;
+
+import java.util.Scanner;
+
+public class QuotientWithException {
+        public static int quotient(int number1, int number2) {
+            if (number2 == 0) {
+                throw new ArithmeticException("Divisor cannot be zero");
+            }
+            return number1 / number2;
+        }
+
+        public static void main(String[] args) {
+            Scanner a = new Scanner(System.in);
+            System.out.println("Enter two integers !: ");
+
+            int number1 = a.nextInt();
+
+            int number2 = a.nextInt();
+            try {
+                int result = quotient(number1, number2);
+                System.out.println(number1 + " / " + number2 + " is " + result);
+
+            }
+            catch (ArithmeticException ex) {
+                System.out.println("Execution continues ...");
+            }
+
+        }
+
+}
